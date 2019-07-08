1672b1acfb1d533fbe6aefa481e1e17bc8eaa546
groiq <35578027+groiq@users.noreply.github.com>
2019-07-08 14:14:38 +0200

    set up training database

--- w3resources_exercises.sql
+++ w3resources_exercises.sql
@@ -4,3 +4,13 @@ SQL Exercises from w3resources (Mysql version)
 https://www.w3resource.com/mysql-exercises/
 */
 
+/*
+Exercises on Data Manipulation Language (DML) & Data Definition Language (DDL)
+*/
+
+/* First round of exercises apparently requires 
+a new database, initially empty. */
+create database if not exists w3resource_selfmade;
+
+use w3resource_selfmade;
+
