1db0bc9d951fe0f3186bfa09b8051c01ff9c49c0
groiq <35578027+groiq@users.noreply.github.com>
2019-07-11 14:12:08 +0200

    tournament sim: insert data

--- various_exercises.sql
+++ various_exercises.sql
@@ -31,3 +31,46 @@ CREATE TABLE contestant (
         REFERENCES contestant (number)
 );
 
+/*
+Code for generating input statements:
+
+names = "Anfroy Bodoloff Chrodegang Drimylos Engelke Folcard Gilpin Hegesandros Ixidor Jorin Kendrick Leudbald Meginhard Nidungus Orkatz Piligrim Quenall Raynoldus Sigibuld Theutgaud Unigild Vitigis Widigast Xabier Ypolitus Zindel"
+names = names.split()
+#print(names)
+
+hitpointList = "11	2	10	1	5	9	4	13	16	7	12	6	8	15	14	3".split()
+for i in range(len(hitpointList)):
+    hitpointList[i] = int(hitpointList[i])
+#print(hitpointList)
+
+for i in range(len(hitpointList)):
+    print("insert into contestant (name,hitpoints) values (\'{}\',{});".format(names[i],hitpointList[i]))
+
+*/
+
+/*
+Names taken from https://www.mithrilandmages.com/utilities/MedievalBrowse.php
+Hitpoint list ranomized using random.org
+*/
+
+insert into contestant (name,hitpoints) values ('Anfroy',11);
+insert into contestant (name,hitpoints) values ('Bodoloff',2);
+insert into contestant (name,hitpoints) values ('Chrodegang',10);
+insert into contestant (name,hitpoints) values ('Drimylos',1);
+insert into contestant (name,hitpoints) values ('Engelke',5);
+insert into contestant (name,hitpoints) values ('Folcard',9);
+insert into contestant (name,hitpoints) values ('Gilpin',4);
+insert into contestant (name,hitpoints) values ('Hegesandros',13);
+insert into contestant (name,hitpoints) values ('Ixidor',16);
+insert into contestant (name,hitpoints) values ('Jorin',7);
+insert into contestant (name,hitpoints) values ('Kendrick',12);
+insert into contestant (name,hitpoints) values ('Leudbald',6);
+insert into contestant (name,hitpoints) values ('Meginhard',8);
+insert into contestant (name,hitpoints) values ('Nidungus',15);
+insert into contestant (name,hitpoints) values ('Orkatz',14);
+insert into contestant (name,hitpoints) values ('Piligrim',3);
+
+
+select * from contestant;
+
+
