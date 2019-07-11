4fc8d1001a22d3c3edd6260a71803d11cc508d73
groiq <35578027+groiq@users.noreply.github.com>
2019-07-11 15:23:40 +0200

    data cube setup stopped for now

--- datacube_basics.sql
+++ datacube_basics.sql
@@ -24,3 +24,23 @@ select cname,iname,quantity,sum(quantity) from customer join purchase using(cid)
 
 select cid,iid,sum(quantity) from purchase P group by cid,iid with rollup;
 
+/* set up a three-dimensional cube */
+
+drop table if exists t1;
+drop table if exists t2;
+drop table if exists t3;
+
+create table t1 (id1 int, name1 varchar(8));
+create table t2 (id2 int, name2 varchar(8));
+create table t3 (id3 int, name3 varchar(8));
+
+insert into t1 values(1,'A');
+insert into t1 values(2,'B');
+insert into t2 values(1,'C');
+insert into t2 values(2,'D');
+insert into t3 values(1,'E');
+insert into t3 values(2,'F');
+
+create table t3dim (id1 int, id2 int, id3 int, value varchar(8));
+
+/* (stopping there for now) */
