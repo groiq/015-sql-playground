3fb27d614ad38d37d2f3dd7270aca6ed6ca39c3e
groiq <35578027+groiq@users.noreply.github.com>
2020-12-31 19:45:29 +0100

    fibonacci: finalized

--- tsql/fibonacci-multistatement.sql
+++ tsql/fibonacci-multistatement.sql
@@ -19,6 +19,8 @@ step int
 with schemabinding 
 as 
 begin
+	declare @step as int = 1;
+
 	-- insert first two lines
 	insert into @fibonacci
 		(pre1, pre2, curr, step)
@@ -26,6 +28,17 @@ begin
 		((2 * @zero - @one), (@one - @zero), @zero, 0),
 		((@one - @zero), @zero, @one, 1);
 
+	-- do the loop
+	while @step < @limit 
+	begin
+		set @step += 1;
+		insert into @fibonacci
+			(pre2, pre1, curr, step)
+		select 
+			pre1, curr, (pre1 + curr), @step 
+		from @fibonacci 
+		where step = @step - 1;
+	end;
 return;
 end;
 go
