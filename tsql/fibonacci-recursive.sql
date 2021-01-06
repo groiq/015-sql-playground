6ec7e4de9f2f9256397219c5faf4388ce1d7d739
groiq <35578027+groiq@users.noreply.github.com>
2021-01-06 11:06:52 +0100

    added params

--- tsql/fibonacci-recursive.sql
+++ tsql/fibonacci-recursive.sql
@@ -8,11 +8,11 @@ use doodles;
 drop function if exists dbo.fibonacciRCTE;
 go
 
-create or alter function dbo.fibonacciRCTE ()
+create or alter function dbo.fibonacciRCTE (@f0 as int, @f1 as int, @min as int, @max as int)
 returns table
 with schemabinding
 as 
-	return (select 'hello world!' as message);
+	return (select @f0 as num);
 go
 
-select * from dbo.fibonacciRCTE();
+select * from dbo.fibonacciRCTE(0, 1, -5, 5);
