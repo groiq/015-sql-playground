92016337061225ae99a1d44dc61c2316783e03b6
groiq <35578027+groiq@users.noreply.github.com>
2021-01-06 11:38:47 +0100

    done recursive fibonacci thing

--- tsql/fibonacci-recursive.sql
+++ tsql/fibonacci-recursive.sql
@@ -12,7 +12,23 @@ create or alter function dbo.fibonacciRCTE (@f0 as int, @f1 as int, @min as int,
 returns table
 with schemabinding
 as 
-	return (select @f0 as num);
+return
+(
+	with fiborec as 
+		(
+			select @f0 as pparent, @f1 as parent, (@f0 + @f1) as curr, 2 as step
+		union
+			select parent as pparent, curr as parent, (parent + curr) as curr, (step + 1) as step 
+			from fiborec 
+			where step < @max
+		--union
+		--	select (parent - pparent) as pparent, pparent as parent, parent as curr, (step - 1) as step
+		--	from fiborec 
+		--	where step > @min
+		)
+
+	select curr, step from fiborec
+);
 go
 
-select * from dbo.fibonacciRCTE(0, 1, -5, 5);
+select * from dbo.fibonacciRCTE(0, 1, 2, 3) order by step;
